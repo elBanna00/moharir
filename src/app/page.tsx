8b29b058f931be0c9cff62c05ca96b12f4e6ec8e
elBanna00 <moaazibrahim3@gmail.com>
2024-05-15 15:00:30 +0300

    style:Add editor header with styling
    - Adds modetoggle button to editor header
    - Adds language select menu
    - Adds flex styling to editor header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ interface pageProps {}
 
 const Home: React.FC<pageProps> = ({}) => {
   return (
-    <div className="dark:bg-zinc-800 bg-slate-200">
+    <div className="dark:bg-gradient-to-l from-slate-950 bg-slate-800 p-8">
       <EditorPane />
     </div>
   );
--- src/components/EditorPane.tsx
+++ src/components/EditorPane.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { ModeToggle } from "./modeToggle";
 
 interface editorPaneProps {}
 
 const EditorPane: React.FC<editorPaneProps> = ({}) => {
   return (
-    <div>
-      <h2>Editor</h2>
+    <div className="min-h-screen dark:bg-black bg-slate-300 rounded-2xl shadow-2xl py-6 px-8">
+      <div className="flex items-center justify-between">
+        <h2 className="scroll-m-20  text-2xl font-semibold tracking-tight first:mt-0">
+          المحرر
+        </h2>
+        <div className="flex">
+          <ModeToggle />
+        </div>
+      </div>
     </div>
   );
 };
