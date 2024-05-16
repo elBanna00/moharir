e1e2d6bfbf8b168197845dfa1ecf305c3c221190
elBanna00 <moaazibrahim3@gmail.com>
2024-05-16 06:41:20 +0300

    add language select menu to the editor header

--- src/components/EditorPane.tsx
+++ src/components/EditorPane.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ModeToggle } from "./modeToggle";
+import LanguagesSelect from "./LanguagesSelect";
 
 interface editorPaneProps {}
 
@@ -10,8 +11,11 @@ const EditorPane: React.FC<editorPaneProps> = ({}) => {
         <h2 className="scroll-m-20  text-2xl font-semibold tracking-tight first:mt-0">
           المحرر
         </h2>
-        <div className="flex">
+        <div className="flex items-center space-x-2 ">
           <ModeToggle />
+          <div className="w-[230px]">
+            <LanguagesSelect />
+          </div>
         </div>
       </div>
     </div>
