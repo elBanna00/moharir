bceced1e9fd5ad2252cc8ae242166158b68eaefc
elBanna00 <moaazibrahim3@gmail.com>
2024-05-18 09:44:06 +0300

    Add initial code snipppets for each lang in select menu

--- src/components/LanguagesSelect.tsx
+++ src/components/LanguagesSelect.tsx
@@ -2,6 +2,7 @@
 import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { LanguangeOpts } from "@/config/config";
 
 const people = [
   {
@@ -71,7 +72,7 @@ function classNames(...classes: string[]) {
 }
 
 export default function LanguagesSelect() {
-  const [selected, setSelected] = useState(people[3]);
+  const [selected, setSelected] = useState(LanguangeOpts[0]);
 
   return (
     <Listbox value={selected} onChange={setSelected}>
@@ -80,12 +81,9 @@ export default function LanguagesSelect() {
           <div className="relative ">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-2 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
               <span className="flex items-center">
-                <img
-                  src={selected.avatar}
-                  alt=""
-                  className="h-5 w-5 flex-shrink-0 rounded-full"
-                />
-                <span className="ml-3 block truncate">{selected.name}</span>
+                <span className="ml-3 block truncate capitalize">
+                  {selected.language}({selected.version})
+                </span>
               </span>
               <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
                 <ChevronUpDownIcon
@@ -103,32 +101,27 @@ export default function LanguagesSelect() {
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {people.map((person) => (
+                {LanguangeOpts.map((lang) => (
                   <Listbox.Option
-                    key={person.id}
+                    key={lang.language}
                     className={({ active }) =>
                       classNames(
                         active ? "bg-indigo-600 text-white" : "text-gray-900",
                         "relative cursor-default select-none py-2 pl-3 pr-9"
                       )
                     }
-                    value={person}
+                    value={lang}
                   >
                     {({ selected, active }) => (
                       <>
                         <div className="flex items-center">
-                          <img
-                            src={person.avatar}
-                            alt=""
-                            className="h-5 w-5 flex-shrink-0 rounded-full"
-                          />
                           <span
                             className={classNames(
                               selected ? "font-semibold" : "font-normal",
-                              "ml-3 block truncate"
+                              "ml-3 block truncate  capitalize"
                             )}
                           >
-                            {person.name}
+                            {lang.language}({lang.version})
                           </span>
                         </div>
 
---
+++ src/config/config.ts
@@ -0,0 +1,128 @@
+export const LanguangeOpts = [
+  {
+    language: "typescript",
+    version: "1.32.3",
+    aliases: ["deno", "deno-ts"],
+    runtime: "deno",
+  },
+  {
+    language: "javascript",
+    version: "1.32.3",
+    aliases: ["deno-js"],
+    runtime: "deno",
+  },
+  {
+    language: "c++",
+    version: "10.2.0",
+    aliases: ["cpp", "g++"],
+    runtime: "gcc",
+  },
+  {
+    language: "go",
+    version: "1.16.2",
+    aliases: ["go", "golang"],
+  },
+  {
+    language: "lua",
+    version: "5.4.4",
+    aliases: [],
+  },
+  {
+    language: "rust",
+    version: "1.68.2",
+    aliases: ["rs"],
+  },
+  {
+    language: "zig",
+    version: "0.10.1",
+    aliases: [],
+  },
+];
+
+export const exampleSnippets = {
+  typescript: `
+  function sum(a: number, b: number): number {
+    const result = a + b;
+    console.log("Sum is: " + result);
+    return result;
+  }
+  sum(1, 2);
+  `.trim(),
+
+  javascript: `
+  function sum(a, b) {
+    const result = a + b;
+    console.log("Sum is: " + result);
+    return result;
+  }
+  sum(1, 2);
+  `.trim(),
+
+  "c++": `
+  #include <iostream>
+  using namespace std;
+  
+  int sum(int a, int b) {
+    int result = a + b;
+    cout << "Sum is: " << result << endl;
+    return result;
+  }
+  
+  int main() {
+    sum(1, 2);
+    return 0;
+  }
+  `.trim(),
+
+  go: `
+  package main
+  
+  import "fmt"
+  
+  func sum(a int, b int) int {
+    result := a + b
+    fmt.Println("Sum is:", result)
+    return result
+  }
+  
+  func main() {
+    sum(1, 2)
+  }
+  `.trim(),
+
+  lua: `
+  function sum(a, b)
+    local result = a + b
+    print("Sum is: " .. result)
+    return result
+  end
+  
+  sum(1, 2)
+  `.trim(),
+
+  rust: `
+  fn sum(a: i32, b: i32) -> i32 {
+    let result = a + b;
+    println!("Sum is: {}", result);
+    return result;
+  }
+  
+  fn main() {
+    sum(1, 2);
+  }
+  `.trim(),
+
+  zig: `
+  const std = @import("std");
+  
+  fn sum(a: i32, b: i32) i32 {
+    const result = a + b;
+    std.debug.print("Sum is: {}\n", .{result});
+    return result;
+  }
+  
+  pub fn main() void {
+    sum(1, 2);
+  }
+  `.trim(),
+};
